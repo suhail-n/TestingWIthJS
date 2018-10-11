57b77735208746993b5b6bb7634fc48ee9b0e386
Suhail-n <suhail.nooristani@gmail.com>
2018-10-11 11:44:07 -0400

    updated tests for the getPrice method

--- unit-testing-and-tdd-nodejs/supermarket/test/checkout.spec.js
+++ unit-testing-and-tdd-nodejs/supermarket/test/checkout.spec.js
@@ -1,6 +1,11 @@
-const should = require("chai").should();
+const chai = require("chai");
+const should = chai.should();
 const expect = require("chai").expect;
+const sinon = require('sinon');
+var sinonChai = require("sinon-chai");
+chai.use(sinonChai);
 const Checkout = require("../checkout");
+const fs = require("fs");
 // x can create an instance of checkout class
 // x can add an item price
 // x can add an item
@@ -8,15 +13,23 @@ const Checkout = require("../checkout");
 // x can add multiple items and get correct total
 // x can add discount rules
 // x can apply discount rules to the total
-// Exception is thrown for item added without price
+// x Exception is thrown for item added without price
 
 describe("Checkout", _ => {
   let checkout;
+  let fileStub;
+
   beforeEach(function() {
     checkout = new Checkout();
     checkout.addItemPrice("a", 1);
     checkout.addItemPrice("b", 2);
+    fileStub = sinon.stub(fs, "readFile").callsFake(function(path, encoding, cb){
+      cb(undefined, '{"a": "2", "d": "1", "c": "4"}');
+    });
   });
+  afterEach(function(){
+    sinon.restore();
+  })
   //   it("can add an item price", function() {
   //     checkout.addItemPrice("chips", 2);
   //   });
@@ -25,27 +38,31 @@ describe("Checkout", _ => {
   //     checkout.addItem("item1");
   //   });
   //   this test is already testing the "can add item price", and "can add an item" so we refactor by removing those
-  it("can calculate the current total", function() {
+  it("should calculate the current total", function() {
     checkout.addItem("a");
     checkout.calculateTotal().should.be.equal(1);
   });
-  it("can add multiple items and get correct total", function() {
+  it("should add multiple items and get correct total", function() {
     checkout.addItem("a");
     checkout.addItem("b");
     checkout.calculateTotal().should.be.equal(3);
   });
-  it("can add discount rules", function() {
+  it("should add discount rules", function() {
     checkout.addDiscount("a", 3, 2);
   });
-  it("can apply discount rules to the total", function() {
+  it("should apply discount rules to the total", function() {
     checkout.addItem("a");
     checkout.addItem("a");
     checkout.addItem("a");
     checkout.addDiscount("a", 3, 2);
-    console.log("Total price: " + checkout.calculateTotal());
     checkout.calculateTotal().should.be.equal(2);
   });
   it("throws an exception when an item is added without price", function() {
     should.Throw(_ => checkout.addItem("d"));
   });
+  // crashes here
+  it("should get item prices from a file", function(){
+    checkout.getPrices("./prices.json");
+    fileStub.should.have.been.calledOnce;
+  })
 });
